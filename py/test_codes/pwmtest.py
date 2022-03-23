83e43a144e95381f2fdfa64ce7296ca9d2dd6d2d
VR Driving Simulator <101948671+drivingsimulator22@users.noreply.github.com>
2022-03-23 15:48:12 +0200

    Add files via upload

---
+++ py/test_codes/pwmtest.py
@@ -0,0 +1,8 @@
+# def pistonmapRange(value, inMin, inMax, outMin, outMax):
+#     return outMin + (((value - inMin) / (inMax - inMin)) * (outMax - outMin))
+# pwm=[2,2,2,2,2,2]
+# def PWWMfun (pwm):
+#  for i in range (1,6):
+#    pwm[i]=pistonmapRange(pistonlength[i],0,400,0,255)
+#    print(pwm[i])
+    
