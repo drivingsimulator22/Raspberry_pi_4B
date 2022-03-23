f024b1f1bf077dd4fe865989d525e43e6279fb10
VR Driving Simulator <101948671+drivingsimulator22@users.noreply.github.com>
2022-03-23 15:41:47 +0200

    Update spi_mcp3008.py

--- py/sensors/pi+mcp3008/spi_mcp3008.py
+++ py/sensors/pi+mcp3008/spi_mcp3008.py
@@ -10,8 +10,16 @@ import Adafruit_GPIO as GPIO
 import Adafruit_MCP3008 as MCP
 import Adafruit_GPIO.SPI as SPI
 import time
+#import translate as map
+#from scipy.interpolate import interp 
 
+#
+# Function for Sensor Readings Mapping
+#
+def sensor_map(value, inMin, inMax, outMin, outMax):
+    return outMin + (((value - inMin) / (inMax - inMin)) * (outMax - outMin))
 
+#
 # Software SPI Configuration
 #
 #CLK = 23
@@ -20,8 +28,10 @@ import time
 #CS = 24
 #mcp = MCP.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
+#
 # Hardware SPI configuration:
-SPI_PORT   = 0
+#
+SPI_PORT = 0
 SPI_DEVICE = 0
 mcp = MCP.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
@@ -38,16 +48,23 @@ while True:
         # The read_adc function will get the value of the specified channel (0-7).
         values[i] = mcp.read_adc(i)
 
-    #values[0] = (1.923 * values[7]) - 84        #Sensor_1 Mapping + Calibration of Zero Error  //1851 mm
-    #values[1] = (0.488888 * values[0]) - 40     #Sesnor_2 Mapping + Calibration of Zero Error  //460 mm
+    #values[0] = (1.923 * values[0]) - 84        #Sensor_1 Mapping + Calibration of Zero Error  //1851 mm
+    #values[1] = (0.484000 * values[1]) - 33     #Sesnor_2 Mapping + Calibration of Zero Error  //460 mm
     #values[2] = (0.488288 * values[2]) - 0     #Sesnor_3 Mapping + Calibration of Zero Error  //500 mm
-    #values[3] = (0.488288 * values[2]) - 0     #Sesnor_4 Mapping + Calibration of Zero Error  //500 mm
-    #values[4] = (0.488288 * values[2]) - 0     #Sesnor_5 Mapping + Calibration of Zero Error  //500 mm
-    #values[5] = (0.488288 * values[2]) - 0     #Sesnor_6 Mapping + Calibration of Zero Error  //500 mm
+    #values[3] = (0.488288 * values[3]) - 0     #Sesnor_4 Mapping + Calibration of Zero Error  //500 mm
+    #values[4] = (0.488288 * values[4]) - 0     #Sesnor_5 Mapping + Calibration of Zero Error  //500 mm
+    #values[5] = (0.488288 * values[5]) - 0     #Sesnor_6 Mapping + Calibration of Zero Error  //500 mm
+
+    values[0] = sensor_map(values[0], 39,  980, 0, 1851) - 7
+    values[1] = sensor_map(values[1], 78, 1023, 0,  466) 
+    values[2] = sensor_map(values[2],  0, 1023, 0,  520)
+    values[3] = sensor_map(values[3],  0, 1023, 0,  500)
+    values[4] = sensor_map(values[4],  0, 1023, 0,  500)
+    values[5] = sensor_map(values[5],  0, 1023, 0,  500) 
+
 
     Values = [int(values) for values in values]
     # Print the ADC values.
     print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*Values))
     # Pause for half a second.
     time.sleep(0.5)
-
