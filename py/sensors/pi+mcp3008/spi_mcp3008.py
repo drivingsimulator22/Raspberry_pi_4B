5e5ca509b64106986bdb8dcc92df3d23e67d48e2
VR Driving Simulator <101948671+drivingsimulator22@users.noreply.github.com>
2022-03-28 14:08:38 +0200

    Update spi_mcp3008.py

--- py/sensors/pi+mcp3008/spi_mcp3008.py
+++ py/sensors/pi+mcp3008/spi_mcp3008.py
@@ -6,65 +6,70 @@
 #      and MCP3008 Chip
 ###
 
-import Adafruit_GPIO as GPIO
+
+#####################_USED_LIBRARY_###############################
 import Adafruit_MCP3008 as MCP
 import Adafruit_GPIO.SPI as SPI
-import time
-#import translate as map
-#from scipy.interpolate import interp 
+################################################################
 
-#
-# Function for Sensor Readings Mapping
-#
+######################Variable_decleration#######################
+#list of the sensors zero error 
+Sensor_Zero_Errors=[7,0,0,0,0,0]   
+################################################################
+
+#######################Functions_definitions####################
+
+# ###############################################################
+# Function Name: Sensor_map
+# Description: Function for Sensor Readings Mapping
+# Inputs:  value-->> the value to be mapped on new range
+#          inMin-->> the mim value in the old range
+#          inMax-->> the max value in the old range
+#          outMin-->> the mim value in the new range
+#          outMin-->> the max value in the new range
+# Output:  the mapped value within the new range
+#################################################################
+
 def sensor_map(value, inMin, inMax, outMin, outMax):
     return outMin + (((value - inMin) / (inMax - inMin)) * (outMax - outMin))
 
-#
-# Software SPI Configuration
-#
-#CLK = 23
-#MISO = 19
-#MOSI = 21
-#CS = 24
-#mcp = MCP.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+##################################################################
+# Function Name: SPI_MCP3008_Init()
+# description: Hardware SPI configuration 
+###################################################################
 
-#
-# Hardware SPI configuration:
-#
-SPI_PORT = 0
-SPI_DEVICE = 0
-mcp = MCP.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+def SPI_MCP3008_Init():
+    global mcp                                                 # to be created as object from the MCP class
+    SPI_PORT = 0                                               # SPI0 module in Raspberry Pi 
+    SPI_DEVICE = 0                                             # SPI0.0 first and only slave     
+    mcp = MCP.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))    # configure the MCP class
 
+####################################################################
+# Function Name: ADC_MCP3008_Readings()
+# description: takes reading from all 6 wire potentiometers from the 
+#              MCP3008 ADC module channels then map the reading from
+#              0 ---> 1023  
+# output : list of all the 6 sensors current readings 
+#####################################################################
 
-print('Reading MCP3008 values, press Ctrl-C to quit...')
-# Print nice channel column headers.
-print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
-print('-' * 57)
-# Main program loop.
-while True:
-    # Read all the ADC channel values in a list.
-    values = [0]*8
+def ADC_MCP3008_Readings():
+    # Read all the 8 ADC channel values in a list.
+    Sensor_Readings = [0]*8
     for i in range(8):
         # The read_adc function will get the value of the specified channel (0-7).
-        values[i] = mcp.read_adc(i)
+        Sensor_Readings[i] = mcp.read_adc(i)
 
-    #values[0] = (1.923 * values[0]) - 84        #Sensor_1 Mapping + Calibration of Zero Error  //1851 mm
-    #values[1] = (0.484000 * values[1]) - 33     #Sesnor_2 Mapping + Calibration of Zero Error  //460 mm
-    #values[2] = (0.488288 * values[2]) - 0     #Sesnor_3 Mapping + Calibration of Zero Error  //500 mm
-    #values[3] = (0.488288 * values[3]) - 0     #Sesnor_4 Mapping + Calibration of Zero Error  //500 mm
-    #values[4] = (0.488288 * values[4]) - 0     #Sesnor_5 Mapping + Calibration of Zero Error  //500 mm
-    #values[5] = (0.488288 * values[5]) - 0     #Sesnor_6 Mapping + Calibration of Zero Error  //500 mm
+   
+# mapping and calibration of every reading 
+    Sensor_Readings[0] = sensor_map(Sensor_Readings[0], 39,  980, 0, 1851) - Sensor_Zero_Errors[0]
+    Sensor_Readings[1] = sensor_map(Sensor_Readings[1], 78, 1023, 0,  466) - Sensor_Zero_Errors[1]
+    Sensor_Readings[2] = sensor_map(Sensor_Readings[2],  0, 1023, 0,  520) - Sensor_Zero_Errors[2]
+    Sensor_Readings[3] = sensor_map(Sensor_Readings[3],  0, 1023, 0,  500) - Sensor_Zero_Errors[3]
+    Sensor_Readings[4] = sensor_map(Sensor_Readings[4],  0, 1023, 0,  500) - Sensor_Zero_Errors[4]
+    Sensor_Readings[5] = sensor_map(Sensor_Readings[5],  0, 1023, 0,  500) - Sensor_Zero_Errors[5]
 
-    values[0] = sensor_map(values[0], 39,  980, 0, 1851) - 7
-    values[1] = sensor_map(values[1], 78, 1023, 0,  466) 
-    values[2] = sensor_map(values[2],  0, 1023, 0,  520)
-    values[3] = sensor_map(values[3],  0, 1023, 0,  500)
-    values[4] = sensor_map(values[4],  0, 1023, 0,  500)
-    values[5] = sensor_map(values[5],  0, 1023, 0,  500) 
-
-
-    Values = [int(values) for values in values]
-    # Print the ADC values.
-    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*Values))
-    # Pause for half a second.
-    time.sleep(0.5)
+# convert all reading from floats to integers and save them in new list
+    Values = [int(values) for values in Sensor_Readings]
+    
+    return Values
+    
